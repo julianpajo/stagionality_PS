64dc9498eadeeea6fd8175ad17976e6784f4c243
Julian Pajo <j.pajo@studenti.uniba.it>
2024-05-21 18:56:44 +0000

    feat: added script for download data

--- services/db_ps/download.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Scarica il file da Google Drive utilizzando gdown
-pip install gdown
-
-mkdir -p src/development/data
-
-gdown --id 1fnBiBr-jFcvomP5UbHaOpw62EzDxuzhs --output src/development/data/ps_measurements.csv
--- services/db_ps/src/development/04_import_data.sql
+++
@@ -1,3 +0,0 @@
-\connect "EULER"
-
-COPY euler.ps_measurements FROM '/docker-entrypoint-initdb.d/data/ps_measurements.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', QUOTE '"');
